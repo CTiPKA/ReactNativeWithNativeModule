e15fb6ada4e6a47f07184d5d30963d1ea8c4a213
Vadim Trulyaev <trulyaev@gmail.com>
2016-12-19 14:32:54 +0300

    Initial commit

---
+++ ios/ProjectWithNewModule/CalendarManager.h
@@ -0,0 +1,14 @@
+//
+//  CalendarManager.h
+//  ProjectWithNewModule
+//
+//  Created by vtrulyaev on 12/19/16.
+//  Copyright © 2016 Facebook. All rights reserved.
+//
+
+
+#import "RCTBridgeModule.h"
+
+@interface CalendarManager : NSObject <RCTBridgeModule>
+
+@end
